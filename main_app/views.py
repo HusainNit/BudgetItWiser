7d8f108a3f59ed8aa1404c91fc9368a9e2e16971
HusainNit <vaintime21@gmail.com>
2025-07-27 11:19:23 +0300

    [ADD] jwt auth: created the jwt authenticate process and test it in postman

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Budget, Expense
-from .serializers import BudgetSerializer, ExpenseSerializer
-from rest_framework import viewsets
+from .serializers import BudgetSerializer, ExpenseSerializer ,SignUpSerializer
+from rest_framework import viewsets , generics
 from django.contrib.auth.models import User
+from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth.models import User
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
 
 # Create your views here.
@@ -10,28 +13,27 @@ from django.contrib.auth.models import User
 class BudgetViewSet(viewsets.ModelViewSet):
     queryset = Budget.objects.all()
     serializer_class = BudgetSerializer
+    permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer): # fix it later as serializer.save(user=self.request.user)
-        dummy_user = User.objects.first()
-        serializer.save(user=dummy_user)
+    def perform_create(self, serializer): 
+        serializer.save(user=self.request.user)
 
-    # def get_queryset(self):
+    # def get_queryset(self): # return info of the user currently sigin in , for later
     #     return Budget.objects.filter(user=self.request.user)
+
 
 
 class ExpenseViewSet(viewsets.ModelViewSet):
     queryset = Expense.objects.all()
     serializer_class = ExpenseSerializer
+    permission_classes = [IsAuthenticated]
 
-    # def perform_create(self, serializer): # fix it later as serializer.save(user=self.request.user)
-    #     budget_test = Budget.objects.first()
-    #     serializer.save(budget=budget_test)
+    # def get_queryset(self): # return info of the user currently sigin in , for later
+    #     return Expense.objects.filter(user=self.request.user)
 
-    # def get_queryset(self):
-    #     return Expense.objects.filter(budget__user=self.request.user)
 
-    # def perform_create(self, serializer):
-    #     user = self.request.user
-    #     budget = Budget.objects.get(user=user)
-    #     serializer.save(budget=budget)
 
+class SignUpView(generics.CreateAPIView): #only gives me create not update, delete, read
+    queryset = User.objects.all()
+    serializer_class = SignUpSerializer
+    permission_classes = [AllowAny]
